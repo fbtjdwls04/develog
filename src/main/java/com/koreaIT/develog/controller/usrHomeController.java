3ee3d2be873bed6183a82eaf44d7b26e7619a004
fbtjdwls04 <fbtjdwls04@gmail.com>
2024-01-08 22:42:42 +0900

    첫 커밋

---
+++ src/main/java/com/koreaIT/develog/controller/usrHomeController.java
@@ -0,0 +1,18 @@
+package com.koreaIT.develog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class usrHomeController {
+	
+	@RequestMapping("/usr/home/main")
+	public String home() {
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
+}
